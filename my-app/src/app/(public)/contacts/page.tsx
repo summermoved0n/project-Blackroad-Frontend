a24d01a85920c52db8774691cc839c6823b7f888
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2026-05-07 23:53:06 -0230

    join DB and create get endpoint

--- my-app/src/app/(public)/contacts/page.tsx
+++ my-app/src/app/(public)/contacts/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { prisma } from "@/lib/prisma";
 
-export default function page() {
+export default async function page() {
+  const users = await prisma.user.findFirst();
+  const posts = await prisma.post.findMany();
+  console.log(users);
+
   return (
-    <div>Contacts page</div>
-  )
+    <div className="pt-20 bg-[#171717]">
+      <ul className="">
+        {posts.map(({ id, title, published, content }) => (
+          <li
+            key={id}
+            className="text-white border-b border-b-white/30 flex flex-col py-10"
+          >
+            <p>Title: {title}</p>
+            <p>Content: {content}</p>
+            <p>{published ? "😎" : "😒"}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
