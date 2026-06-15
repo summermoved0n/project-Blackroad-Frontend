7b3cc0516c0f4d2500b65263505155838c91ce76
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2026-06-15 19:34:46 -0230

    Revert "app re-name"

--- Blackroad/prisma/migrations/20260529024204_userid_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `bookings` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "bookings_userId_key" ON "bookings"("userId");
